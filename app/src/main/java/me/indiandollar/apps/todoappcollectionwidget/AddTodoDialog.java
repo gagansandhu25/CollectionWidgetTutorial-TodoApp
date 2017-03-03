ee785d43e71e2472f14a1549f7b78a144e7a88fc
Gagandeep Singh <macroose.coder@gmail.com>
2017-03-03 18:11:58 +0530

    Add Todo Dialog implemented.

---
+++ app/src/main/java/me/indiandollar/apps/todoappcollectionwidget/AddTodoDialog.java
@@ -0,0 +1,53 @@
+package me.indiandollar.apps.todoappcollectionwidget;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Indian Dollar on 3/3/2017.
+ */
+
+public class AddTodoDialog extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View custom = inflater.inflate(R.layout.add_todo_dialog, null);
+
+
+        builder.setView(custom);
+
+        builder.setMessage(getString(R.string.todo_dialog_title));
+        builder.setPositiveButton(getString(R.string.todo_dialog_add),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        addTodo();
+                    }
+                });
+        builder.setNegativeButton(getString(R.string.todo_dialog_cancel), null);
+
+        Dialog dialog = builder.create();
+
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        }
+
+        return dialog;
+
+    }
+
+    private void addTodo() {
+    }
+
+
+}
