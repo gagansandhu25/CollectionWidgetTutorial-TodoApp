37ccdfae78dcf90d1a5880f90bac9d0c2ec0e10b
Gagandeep Singh <macroose.coder@gmail.com>
2017-03-05 22:55:27 +0530

    Implemented the db helper to handle the create and drop of database tables.

---
+++ app/src/main/java/me/indiandollar/apps/todoappcollectionwidget/DbHelper.java
@@ -0,0 +1,47 @@
+package me.indiandollar.apps.todoappcollectionwidget;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Indian Dollar on 3/5/2017.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "todoItems.db";
+    private static String SQL_DROP_TABLE;
+    private static String SQL_CREATE_TABLE;
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        SQL_CREATE_TABLE = "CREATE TABLE " +
+                Contract.TABLE_NAME + "(" +
+                Contract._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Contract.COL_TODO_TEXT + " TEXT NOT NULL, " +
+                Contract.COL_TODO_DISPLAY_POSITION + " INTEGER UNIQUE NOT NULL" +
+                ")";
+
+        db.execSQL(SQL_CREATE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        SQL_DROP_TABLE = "DROP TABLE IF EXISTS " + Contract.TABLE_NAME;
+
+        db.execSQL(SQL_DROP_TABLE);
+        onCreate(db);
+
+    }
+}
+
