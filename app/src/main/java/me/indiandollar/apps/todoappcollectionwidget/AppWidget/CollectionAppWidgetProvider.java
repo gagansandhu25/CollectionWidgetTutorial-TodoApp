a10c2b3e9e5048daa278e84bd72a1ee60dc15480
Gagandeep Singh <macroose.coder@gmail.com>
2017-03-22 01:56:36 +0530

    add list item layout for collection widget + defined widget in manifest

---
+++ app/src/main/java/me/indiandollar/apps/todoappcollectionwidget/AppWidget/CollectionAppWidgetProvider.java
@@ -0,0 +1,17 @@
+package me.indiandollar.apps.todoappcollectionwidget.AppWidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Indian Dollar on 3/22/2017.
+ */
+
+public class CollectionAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
